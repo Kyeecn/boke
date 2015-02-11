848975c58b8476b8db9b14c3b26b97d4c9ab432c
Kyee <kyeecn@gmail.com>
2015-02-11 10:52:07 +0800

    add ico .

--- wp-content/themes/twentyfifteen/footer.php
+++ wp-content/themes/twentyfifteen/footer.php
@@ -22,10 +22,14 @@
 				 */
 				do_action( 'twentyfifteen_credits' );
 			?>
-			@2014-2015 <a href='http://www.weibo.com/ChenKaiyuuuu' target='_blank'>Kyee</a> | 
-      <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1254076149'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s11.cnzz.com/z_stat.php%3Fid%3D1254076149' type='text/javascript'%3E%3C/script%3E"));</script> | 
-      已备案 托管于<a href="http://www.aliyun.com/" target="_blank">阿里云</a> |
-      <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyfifteen' ) ); ?>" target="_blank"><?php printf( __( 'Powered by %s', 'twentyfifteen' ), 'WordPress' ); ?></a>
+      <center>
+			  @2014-2015 <a href='http://www.weibo.com/ChenKaiyuuuu' target='_blank'>Kyee</a> | 
+        <a href="http://www.miitbeian.gov.cn/state/outPortal/loginPortal.action" target="_blank">浙ICP备14033785号-1</a> |
+        <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1254076149'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s11.cnzz.com/z_stat.php%3Fid%3D1254076149' type='text/javascript'%3E%3C/script%3E"));</script> <br>
+        Powered by <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyfifteen' ) ); ?>" target="_blank">WordPress</a> | 
+        Designed by <a href="https://wordpress.org/themes/twentyfifteen" target="_blank">Twentyfifteen</a> | 
+        托管于<a href="http://www.aliyun.com/" target="_blank">阿里云</a>
+      </center>
 		</div><!-- .site-info -->
 	</footer><!-- .site-footer -->
 
--- wp-content/themes/twentyfifteen/header.php
+++ wp-content/themes/twentyfifteen/header.php
@@ -13,6 +13,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width">
+  <link rel="shortcut icon" type="image/x-icon" href="/wp-content/uploads/ico/kyee.ico" />
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<!--[if lt IE 9]>
